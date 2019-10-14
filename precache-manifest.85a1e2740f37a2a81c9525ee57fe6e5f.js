fbc21f3efb2d3b8edfefccf91c0d83e1002588a4
DaniilMarch <asmandian@gmail.com>
2019-10-14 12:27:36 +0300

    Updates

---
+++ precache-manifest.85a1e2740f37a2a81c9525ee57fe6e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e7f37ce0407b08d3dfa0b06f3a08db",
+    "url": "/threejs-sandbox/index.html"
+  },
+  {
+    "revision": "e505298a7d967adecfbd",
+    "url": "/threejs-sandbox/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e505298a7d967adecfbd",
+    "url": "/threejs-sandbox/static/js/2.a2f0226f.chunk.js"
+  },
+  {
+    "revision": "784d5cffaf5558c0cacc",
+    "url": "/threejs-sandbox/static/js/main.93d0ae3f.chunk.js"
+  },
+  {
+    "revision": "4bb70ac40dbd3a92e659",
+    "url": "/threejs-sandbox/static/js/runtime-main.bc906bbb.js"
+  }
+]);
